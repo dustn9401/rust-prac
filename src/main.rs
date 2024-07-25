c8815811c796b88cd3a8449b6a3add4fc800f9c8
김연수 <dustn9401@line.games>
2024-07-25 16:28:19 +0900

    guess_game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    let secret_num = rand::thread_rng().gen_range(1..=100);
+
+    println!("secret_num: {secret_num}");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {guess}");
+
+    match guess.cmp(&secret_num) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Equal => println!("You win!"),
+        Ordering::Greater => println!("Too big!"),
+    }
 }
